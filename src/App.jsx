1335fc41f57bee176427bf9bbf0931a07934369e
nivancheva <nedyalka.tsintsarova@gmail.com>
2023-10-26 15:21:30 +0300

    connetct to weather API

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,22 @@ import clearimg from './images/clear.png';
 import mistimg from './images/mist.png';
 import rainimg from './images/rain.png';
 import snowimg from './images/snow.png';
+import { useState } from 'react';
 
 function App() {
+  const APIKey = '';
+  const [showWeather, setShowWeather] = useState(null);
+  const [city, setCity] = useState();
+
+  async function displayWeather() {
+    const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${APIKey}&q=${city}&aqi=no`);
+
+    if(response.ok) {
+      const weather = await response.json();
+      setShowWeather(weather.current);
+      console.log(weather);
+    }
+  }
 
   return (
     <div>
@@ -14,8 +28,8 @@ function App() {
       <div className="container">
         <div className="search-box">
           <i className='bx bxs-map'></i>
-          <input type="text" placeholder='enter your location'/>
-          <button className='bx bx-search'></button>
+          <input type="text" value={city} onChange={(e) => {setCity(e.target.value)}} placeholder='enter your location'/>
+          <button className='bx bx-search' onClick={displayWeather}></button>
         </div>
 
         <div className="weather-box">
@@ -23,8 +37,8 @@ function App() {
             <div className="info-weather">
               <div className="weather">
                 <img src={cloudImg}/>
-                <p className='temperature'>16<span>°C</span></p>
-                <p className="description">Broken Clouds</p>
+                <p className='temperature'>{showWeather?.temp_c}<span>°C</span></p>
+                <p className="description">{showWeather?.condition.text}</p>
               </div>
             </div>
           </div>
@@ -34,7 +48,7 @@ function App() {
               <i className='bx bx-water'></i>
               <div className="text">
                 <div className="info-humidity">
-                  <span>0%</span>
+                  <span>{showWeather?.humidity}%</span>
                 </div>
                 <p>Humidity</p>
               </div>
@@ -44,7 +58,7 @@ function App() {
               <i className='bx bx-wind'></i>
               <div className="text">
                 <div className="info-wind">
-                  <span>0Km/h</span>
+                  <span>{showWeather?.wind_kph}Km/h</span>
                 </div>
                 <p>Wind Speed</p>
               </div>
